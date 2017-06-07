3caf7d118d72de8bf705757da08c2ad67b10885f
Jenkinsk <Jenkinsk4528@student.faytechcc.edu>
2017-06-07 15:53:50 -0400

    Add files via upload
    first submission

---
+++ M2T1_Sales Prediction_KennethJenkinsJr.py
@@ -0,0 +1,16 @@
+Sales prediction table
+6/7/17
+CTI-110 M2T1-Sales Prediction
+Kenneth Jenkins Jr
+
+
+
+
+# Get the predicted total sales.
+total_sales = float (input ('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print ('The profit is $',format (profit, ',.2f'))
